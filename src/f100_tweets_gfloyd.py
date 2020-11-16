591578c96ce3fd707246bc9eda3829a43f52e610
pjrooney <pjrooney.14@gmail.com>
2020-11-16 15:44:47 -0500

    2020-11-16: Adding first cut .csv of base case (T. Cook) tweets

--- src/f100_tweets_gfloyd.py
+++ src/f100_tweets_gfloyd.py
@@ -20,12 +20,13 @@ keys.py should contain the imported variables.
 
 from __future__ import print_function
 
-import json
+import os
+import pandas as pd
 
 import tweepy
 import datetime
 from keys import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-# from accounts import *
+# from account import *
 
 # Creating the authentication object:
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -39,16 +40,33 @@ username = 'tim_cook'
 start_date = datetime.datetime(2020, 5, 4, 0, 0, 0)
 end_date = datetime.datetime(2020, 6, 15, 0, 0, 0)
 
-tweets = {}
+tweets = []
 
-tmpTweets = api.user_timeline(username)  # Get all tweets from user timeline
+tmpTweets = api.user_timeline(username, tweet_mode='extended')  # Get all tweets from user timeline
 for tweet in tmpTweets:
     if tweet.created_at < end_date and tweet.created_at > start_date:
-        tweets.update({tweet.created_at: tweet.text})
-
+        tweets.append(tweet)
 
 while tmpTweets[-1].created_at > start_date:
-    tmpTweets = api.user_timeline(username, max_id = tmpTweets[-1].id)  # Find the id of last tweet
+    tmpTweets = api.user_timeline(username, max_id = tmpTweets[-1].id, tweet_mode='extended')  # Find the id of tweet
     for tweet in tmpTweets:
         if tweet.created_at < end_date and tweet.created_at > start_date:
-            tweets.update({tweet.created_at: tweet.text})
+            tweets.append(tweet)
+
+tweets_df = pd.DataFrame(vars(tweets[i]) for i in range(len(tweets)))
+
+# Select relevant columns
+cols = ['_json', 'created_at', 'id', 'id_str', 'full_text', 'display_text_range', 'entities', 'source', 'user',
+        'retweet_count', 'favorite_count', 'quoted_status_id', 'quoted_status']
+tweets_df = tweets_df[cols]
+
+# Export the df to csv in data directory
+print(os.getcwd())
+
+output_file = 'cook_tweets.csv'
+output_dir = os.path.join(os.getcwd() + '/data/')
+os.makedirs(output_dir, exist_ok=True)
+
+tweets_df.to_csv(os.path.join(output_dir + output_file))
+
+

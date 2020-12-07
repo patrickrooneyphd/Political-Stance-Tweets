4a45a4e3f71a70b52be6976df8568e14631f90a1
pjrooney <pjrooney.14@gmail.com>
2020-12-07 16:48:13 -0500

    2020-12-07: Adding new company and individual Twitter accounts

--- src/accounts.py
+++ src/accounts.py
@@ -4,10 +4,23 @@
 # Desc: Lists of all accounts to scrape
 #
 
-ceos = {"tim_cook": "APPLE INC", "JeffBezos": "AMAZON.COM INC", "sundarpichai": "ALPHABET INC",
+ceos = {"sundarpichai": "ALPHABET INC", "JeffBezos": "AMAZON.COM INC", "tim_cook": "APPLE INC",
         "satyanadella": "MICROSOFT CORP",
         }
 
-companies = {
+companies = {"abbvie": "ABBVIE INC", "Allstate": "ALLSTATE CORP", "Google": "ALPHABET INC",
+             "amazon": "AMAZON.COM INC", "AmericanAir": "AMERICAN AIRLINES GROUP INC",
+             "AmericanExpress": "AMERICAN EXPRESS CO", "AIGinsurance": "AMERICAN INTERNATIONAL GROUP",
+             "Healthcare_ABC": "AMERISOURCEBERGEN CORP", "AnthemInc": "ANTHEM INC", "Apple": "APPLE INC",
+             "ADMupdates": "ARCHER-DANIELS-MIDLAND CO", "ATT": "AT&T INC", "BankofAmerica": "BANK OF AMERICA CORP",
+             "BestBuy": "BEST BUY CO INC", "Boeing": "BOEING CO"
             }
+
+# Interesting Notes:
+# GOOG has public policy team (@googlepubpolicy)
+# ABC has an advocacy Twitter handle (@Advocacy_ABC)
+# AAPL (@Apple) twitter account is empty-- they only buy ads on twitter
+
+# CEOs to Update:
+# BOE: Dave Calhoun, started Jan 13, 2020, on Boeing Board of Dirs since 2009, VP at Blackstone prior to CEO, no Twitter
 

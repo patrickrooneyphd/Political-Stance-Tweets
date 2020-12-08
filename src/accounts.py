2c0b20ab25d3d130326cad8e2605d0aba6e3e959
pjrooney <pjrooney.14@gmail.com>
2020-12-08 17:34:00 -0500

    2020-12-08: Added TWTR accts (ended at Marathon Petroleum, no CEO acct)

--- src/accounts.py
+++ src/accounts.py
@@ -5,6 +5,11 @@
 #
 
 ceos = {"sundarpichai": "ALPHABET INC", "JeffBezos": "AMAZON.COM INC", "tim_cook": "APPLE INC",
+        "ChuckRobbins": "CISCO SYSTEMS INC", "LarryMerloCVS": "CVS HEALTH CORP", "MichaelDell": "DELL TECHNOLOGIES INC",
+        "RobertIger": "DISNEY (WALT) CO", "JimFitterling": "DOW INC", "larryculpjr": "GENERAL ELECTRIC CO",
+        "mtbarra": "GENERAL MOTORS CO", "DavidSolomon": "GOLDMAN SACHS GROUP INC",
+        "Samuel_HazenHCA": "HCA HEALTHCARE INC", "BruceDBroussard": "HUMANA INC", "BobSwan": "INTEL CORP",
+        "GinniRometty": "INTL BUSINESS MACHINES CORP", "MarvinREllison": "LOWE'S COS INC",
         "satyanadella": "MICROSOFT CORP",
         }
 
@@ -13,14 +18,39 @@ companies = {"abbvie": "ABBVIE INC", "Allstate": "ALLSTATE CORP", "Google": "ALP
              "AmericanExpress": "AMERICAN EXPRESS CO", "AIGinsurance": "AMERICAN INTERNATIONAL GROUP",
              "Healthcare_ABC": "AMERISOURCEBERGEN CORP", "AnthemInc": "ANTHEM INC", "Apple": "APPLE INC",
              "ADMupdates": "ARCHER-DANIELS-MIDLAND CO", "ATT": "AT&T INC", "BankofAmerica": "BANK OF AMERICA CORP",
-             "BestBuy": "BEST BUY CO INC", "Boeing": "BOEING CO"
+             "BestBuy": "BEST BUY CO INC", "Boeing": "BOEING CO", "CapitalOne": "CAPITAL ONE FINANCIAL CORP",
+             "cardinalhealth": "CARDINAL HEALTH INC", "CATERPILLAR INC": "CaterpillarInc", "Centene": "CENTENE CORP",
+             "GetSpectrum": "CHARTER COMMUNICATIONS INC", "Chevron": "CHEVRON CORP", "Cigna": "CIGNA CORP",
+             "Cisco": "CISCO SYSTEMS INC", "Citi": "CITIGROUP INC", "CocaColaCo": "COCA-COLA CO",
+             "comcast": "COMCAST CORP", "conocophillips": "CONOCOPHILLIPS", "Costco": "COSTCO WHOLESALE CORP",
+             "CVSHealth": "CVS HEALTH CORP", "JohnDeere": "DEERE & CO", "DellTech": "DELL TECHNOLOGIES INC",
+             "Dell": "DELL TECHNOLOGIES INC", "Delta": "DELTA AIR LINES INC", "WaltDisneyCo": "DISNEY (WALT) CO",
+             "DowNewsroom": "DOW INC", "Exelon": "EXELON CORP", "exxonmobil": "EXXON MOBIL CORP",
+             "Facebook": "FACEBOOK INC", "FreddieMac": "FEDERAL HOME LOAN MORTG CORP",
+             "FannieMae": "FEDERAL NATIONAL MORTGA ASSN", "Ford": "FORD MOTOR CO",
+             "generaldynamics": "GENERAL DYNAMICS CORP", "generalelectric": "GENERAL ELECTRIC CO",
+             "GM": "GENERAL MOTORS CO", "GoldmanSachs": "GOLDMAN SACHS GROUP INC",
+             "HCAhealthcare": "HCA HEALTHCARE INC", "HelmerichPayne": "HELMERICH & PAYNE",
+             "HomeDepot": "HOME DEPOT INC", "honeywell": "HONEYWELL INTERNATIONAL INC", "Humana": "HUMANA INC",
+             "intel": "INTEL CORP", "IBM": "INTL BUSINESS MACHINES CORP", "jnjnews": "JOHNSON & JOHNSON",
+             "jpmorgan": "JPMORGAN CHASE & CO", "kroger": "KROGER CO", "LockheedMartin": "LOCKHEED MARTIN CORP",
+             "Lowes": "LOWE'S COS INC", "MarathonPetroCo": "MARATHON PETROLEUM CORP",
+
+
+
+
+
             }
 
 # Interesting Notes:
 # GOOG has public policy team (@googlepubpolicy)
 # ABC has an advocacy Twitter handle (@Advocacy_ABC)
 # AAPL (@Apple) twitter account is empty-- they only buy ads on twitter
+# CVS (@CVSinAction) is a local community service oriented account
+# XOM (@XOMFoundation) is an account for the company's philanthropic arm
+# Ford (@FordFund) is an account for the company's philanthropic arm
+# Home Depot (@HomeDepotFound) is an account for the company's philanthropic arm
 
-# CEOs to Update:
+# CEOs Notes:
 # BOE: Dave Calhoun, started Jan 13, 2020, on Boeing Board of Dirs since 2009, VP at Blackstone prior to CEO, no Twitter
-
+# @finkd is Mark Zuckerberg's twitter, which is still active but has around 10 posts, most from 2009.

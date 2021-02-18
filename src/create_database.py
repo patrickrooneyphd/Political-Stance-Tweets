c06ee1a9e1e913637fd628ff300df17159ce715b
pjrooney <pjrooney.14@gmail.com>
2021-02-18 12:09:27 -0500

    2021-02-18: Update database creation

--- src/create_database.py
+++ src/create_database.py
@@ -3,11 +3,23 @@
 #
 # Desc: Creates local SQLite Database for Political Stance Tweets Project
 
-import os
-
 import sqlite3
 from sqlite3 import Error
 
-# Change to path in which your SQLite application is located.
-SQLITE_PATH = os.chdir()
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_connection(r"/Users/pjrooney/SQLite/db/political_tweets.db")  # Change to your SQL Directory
 
